097c25666f73625e7cf331c03b76932437a67f94
Fahriddin3040 <fahriddin3040@gmail.com>
2023-11-07 09:08:39 +0500

    MVC REMAKE #2

--- controll.py
+++ controll.py
@@ -8,19 +8,19 @@ class Controller:
 
     model = model.Model()
 
-    def set_expence(self, reason=float, price=float) -> None:
+    def set_expence(self, reason: float, price: float) -> None:
         self.model.set(reason=reason, price=price, mode=Expence)
 
-    def set_income(self, reason=float, price=float) -> None:
+    def set_income(self, reason: float, price: float) -> None:
         self.model.set(reason=reason, price=price, mode=Income)
 
     def get_balance(self) -> float:
         self.model.get_balance()
         return self.model.balance
 
-    def update(self, mode=int, choose=int, reason=float, price=str) -> None:
+    def update(self, mode: int, choose: int, reason: float, price: str) -> None:
         self.model.update(mode=mode, choose=choose, reason=reason, price=price)
 
-    def remove(self, choose=int, mode=int) -> None:
+    def remove(self, choose: int, mode: int) -> None:
         self.model.remove(choose=choose, mode=mode)
 
